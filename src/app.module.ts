8c11883027ce76936b679a79a25ce60fc745fdb5
Sergey Badin <sergeykfg1@me.com>
2026-06-01 12:40:49 +0200

    Configure TypeORM with SQLite driver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ThreadsModule } from './threads/threads.module';
 import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [ThreadsModule, CommentsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: 'data/cyber-chat.sqlite',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ThreadsModule,
+    CommentsModule,
+  ],
 })
 export class AppModule {}
